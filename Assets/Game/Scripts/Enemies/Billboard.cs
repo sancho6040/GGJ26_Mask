d3d66bb40c7d13f716e6ec878767ed7a8f1ed9bb
sancho6040 <qwetito@gmail.com>
2026-02-02 14:49:19 -0500

    build 001
    + player animations

--- Assets/Game/Scripts/Enemies/Billboard.cs
+++ Assets/Game/Scripts/Enemies/Billboard.cs
@@ -6,11 +6,22 @@ public class Billboard : MonoBehaviour
 
     void Start()
     {
+        SetCamera();
+    }
+
+    private void SetCamera()
+    {
         cam = Camera.main;
     }
 
     void LateUpdate()
     {
+        if (cam == null)
+        {
+            SetCamera();
+            print($"camera nor found at: {transform.parent.name}");
+            return;
+        }
         Vector3 dir = transform.position - cam.transform.position;
         dir.y = 0;
         transform.forward = dir;
